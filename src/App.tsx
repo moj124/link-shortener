ebe1829da4ca23536821ba95a18bce1139db37bb
michael jones <michael.jowen.jones@gmail.com>
2021-10-26 23:47:20 +0100

    added checks

--- src/App.tsx
+++ src/App.tsx
@@ -69,192 +69,197 @@ function App(): JSX.Element {
 
   return (
     <Container centerContent>
-    <form onSubmit={handleSubmit(onSubmissionHandler)}>
-      <VStack
-        height="100vh"
-        alignItems="center"
-        justifyContent="center"
-        spacing={10}
-      >
-        <Flex
-          width={{ base: "75%", sm: "75%", md: "110%" }}
-          direction="column"
-          background="white"
-          p={12}
-          rounded={6}
+      <form onSubmit={handleSubmit(onSubmissionHandler)}>
+        <VStack
+          height="100vh"
+          alignItems="center"
+          justifyContent="center"
+          spacing={10}
         >
-          <Heading mb={6}>Link Shortener</Heading>
-          <FormControl isInvalid={errors.url} isRequired>
-            <FormLabel htmlFor="url">URL</FormLabel>
-            <Box>
-              <Input
-                id="url"
-                value={originalURL}
-                aria-invalid={errors.url ? "true" : "false"}
-                {...register("url", {
-                  validate: (value) =>
-                    validateURL(value) || "please enter a valid URL",
-                })}
-                onChange={(e) => {
-                  setURLoriginal(e.target.value);
-                  setURLsubmitted(false);
-                }}
-                autoFocus
-              />
-              {/* use  to announce the error message */}
-              <FormErrorMessage>
-                {errors.url && errors.url.message}
-              </FormErrorMessage>
-            </Box>
-          </FormControl>
-          {!URLsubmitted && (
-            <FormControl isInvalid={errors.addon}>
-              <VStack marginTop="15px" display="flex-start">
-                <FormLabel htmlFor="addon">Customise Link</FormLabel>
-                <InputGroup>
-                  <InputLeftAddon>{process.env.REACT_APP_API}/</InputLeftAddon>
-                  <Input
-                    id="addon"
-                    placeholder="alias"
-                    value={newURL}
-                    onChange={(e) => {
-                      setURLnew(e.target.value);
-                      setURLsubmitted(false);
-                    }}
-                  />
-                </InputGroup>
-              </VStack>
-            </FormControl>
-          )}
-          {!URLsubmitted && (
-            <Box display="flex" justifyContent="space-between" marginTop="15px">
-              <Button
-                isDisabled={URLsubmitted}
-                mb={4}
-                colorScheme="teal"
-                type="submit"
-                isLoading={isSubmitting}
-              >
-                {" "}
-                Shorten{" "}
-              </Button>
-            </Box>
-          )}
-          {/* use aria-invalid to indicate field contain error */}
-          {URLsubmitted && (
-            <>
-              <Box marginTop="15px">
-                <FormLabel>Shortened Link</FormLabel>
-                <InputGroup>
-                  <Input
-                    value={process.env.REACT_APP_API + "/" + newURL}
-                    onChange={(e) => setURLnew(e.target.value)}
-                    variant="outline"
-                    mb={3}
-                    type="text"
-                    readOnly={true}
-                    autoFocus
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button
-                      h="1.75rem"
-                      size="sm"
-                      onClick={onCopy}
-                      colorScheme="teal"
-                    >
-                      {hasCopied ? "Copied" : "Copy"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </Box>
-              <Box display="flex" justifyContent="flex-start">
-                <Button
-                  isDisabled={!URLsubmitted}
-                  mb={4}
-                  colorScheme="teal"
-                  isLoading={isSubmitting}
-                  onClick={(event) => {
-                    window.location.href = `${process.env.REACT_APP_API}/${newURL}`;
-                  }}
-                  marginEnd={3}
-                >
-                  Visit URL
-                </Button>
-                <Button
-                  isDisabled={!URLsubmitted}
-                  mb={4}
-                  colorScheme="teal"
-                  isLoading={isSubmitting}
-                  onClick={() => {
-                    reset({ url: "" });
+          <Flex
+            width={{ base: "75%", sm: "75%", md: "110%" }}
+            direction="column"
+            background="white"
+            p={12}
+            rounded={6}
+          >
+            <Heading mb={6}>Link Shortener</Heading>
+            <FormControl isInvalid={errors.url} isRequired>
+              <FormLabel htmlFor="url">URL</FormLabel>
+              <Box>
+                <Input
+                  id="url"
+                  value={originalURL}
+                  aria-invalid={errors.url ? "true" : "false"}
+                  {...register("url", {
+                    validate: (value) =>
+                      validateURL(value) || "please enter a valid URL",
+                  })}
+                  onChange={(e) => {
+                    setURLoriginal(e.target.value);
                     setURLsubmitted(false);
                   }}
-                  marginEnd={3}
-                >
-                  Shorten another
-                </Button>
+                  autoFocus
+                />
+                {/* use  to announce the error message */}
+                <FormErrorMessage>
+                  {errors.url && errors.url.message}
+                </FormErrorMessage>
+              </Box>
+            </FormControl>
+            {!URLsubmitted && (
+              <FormControl isInvalid={errors.addon}>
+                <VStack marginTop="15px" display="flex-start">
+                  <FormLabel htmlFor="addon">Customise Link</FormLabel>
+                  <InputGroup>
+                    <InputLeftAddon>
+                      {process.env.REACT_APP_API}/
+                    </InputLeftAddon>
+                    <Input
+                      id="addon"
+                      placeholder="alias"
+                      value={newURL}
+                      onChange={(e) => {
+                        setURLnew(e.target.value);
+                        setURLsubmitted(false);
+                      }}
+                    />
+                  </InputGroup>
+                </VStack>
+              </FormControl>
+            )}
+            {!URLsubmitted && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                marginTop="15px"
+              >
                 <Button
-                  isDisabled={!URLsubmitted}
+                  isDisabled={URLsubmitted}
                   mb={4}
                   colorScheme="teal"
+                  type="submit"
                   isLoading={isSubmitting}
-                  onClick={() => setShowQRCode(!showQRCode)}
                 >
-                  Share
+                  {" "}
+                  Shorten{" "}
                 </Button>
               </Box>
-              {showQRCode && (
-                <Box flex-direction="row" display="flex">
-                  <QRCode
-                    value={`${process.env.REACT_APP_API}/${newURL}`}
-                    size={200}
-                  />
-                  <VStack paddingLeft={3} alignItems="none">
-                    <FacebookShareButton
-                      url={`${process.env.REACT_APP_API}/${newURL}`}
-                      quote="Click Here"
-                      className="custom-share-button"
-                    >
-                      <FacebookIcon size={35} className="custom-share-icon" />
-                      <p>Facebook</p>
-                    </FacebookShareButton>
-                    <TwitterShareButton
-                      url={`${process.env.REACT_APP_API}/${newURL}`}
-                      className="custom-share-button"
-                    >
-                      <TwitterIcon size={35} className="custom-share-icon" />
-                      <p>Twitter</p>
-                    </TwitterShareButton>
-                    <WhatsappShareButton
-                      url={`${process.env.REACT_APP_API}/${newURL}`}
-                      className="custom-share-button"
-                    >
-                      <WhatsappIcon size={35} className="custom-share-icon" />
-                      <p>Whatsapp</p>
-                    </WhatsappShareButton>
-                    <LinkedinShareButton
-                      url={`${process.env.REACT_APP_API}/${newURL}`}
-                      className="custom-share-button"
-                    >
-                      <LinkedinIcon size={35} className="custom-share-icon" />
-                      <p>Linkedin</p>
-                    </LinkedinShareButton>
-                    <EmailShareButton
-                      url={`${process.env.REACT_APP_API}/${newURL}`}
-                      className="custom-share-button"
-                    >
-                      <EmailIcon size={35} className="custom-share-icon" />
-                      <p>Email</p>
-                    </EmailShareButton>
-                  </VStack>
+            )}
+            {/* use aria-invalid to indicate field contain error */}
+            {URLsubmitted && (
+              <>
+                <Box marginTop="15px">
+                  <FormLabel>Shortened Link</FormLabel>
+                  <InputGroup>
+                    <Input
+                      value={process.env.REACT_APP_API + "/" + newURL}
+                      onChange={(e) => setURLnew(e.target.value)}
+                      variant="outline"
+                      mb={3}
+                      type="text"
+                      readOnly={true}
+                      autoFocus
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button
+                        h="1.75rem"
+                        size="sm"
+                        onClick={onCopy}
+                        colorScheme="teal"
+                      >
+                        {hasCopied ? "Copied" : "Copy"}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
                 </Box>
-              )}
-            </>
-          )}
-        </Flex>
-      </VStack>
-    </form>
-
+                <Box display="flex" justifyContent="flex-start">
+                  <Button
+                    isDisabled={!URLsubmitted}
+                    mb={4}
+                    colorScheme="teal"
+                    isLoading={isSubmitting}
+                    onClick={(event) => {
+                      window.location.href = `${process.env.REACT_APP_API}/${newURL}`;
+                    }}
+                    marginEnd={3}
+                  >
+                    Visit URL
+                  </Button>
+                  <Button
+                    isDisabled={!URLsubmitted}
+                    mb={4}
+                    colorScheme="teal"
+                    isLoading={isSubmitting}
+                    onClick={() => {
+                      reset({ url: "" });
+                      setURLsubmitted(false);
+                    }}
+                    marginEnd={3}
+                  >
+                    Shorten another
+                  </Button>
+                  <Button
+                    isDisabled={!URLsubmitted}
+                    mb={4}
+                    colorScheme="teal"
+                    isLoading={isSubmitting}
+                    onClick={() => setShowQRCode(!showQRCode)}
+                  >
+                    Share
+                  </Button>
+                </Box>
+                {showQRCode && (
+                  <Box flex-direction="row" display="flex">
+                    <QRCode
+                      value={`${process.env.REACT_APP_API}/${newURL}`}
+                      size={200}
+                    />
+                    <VStack paddingLeft={3} alignItems="none">
+                      <FacebookShareButton
+                        url={`${process.env.REACT_APP_API}/${newURL}`}
+                        quote="Click Here"
+                        className="custom-share-button"
+                      >
+                        <FacebookIcon size={35} className="custom-share-icon" />
+                        <p>Facebook</p>
+                      </FacebookShareButton>
+                      <TwitterShareButton
+                        url={`${process.env.REACT_APP_API}/${newURL}`}
+                        className="custom-share-button"
+                      >
+                        <TwitterIcon size={35} className="custom-share-icon" />
+                        <p>Twitter</p>
+                      </TwitterShareButton>
+                      <WhatsappShareButton
+                        url={`${process.env.REACT_APP_API}/${newURL}`}
+                        className="custom-share-button"
+                      >
+                        <WhatsappIcon size={35} className="custom-share-icon" />
+                        <p>Whatsapp</p>
+                      </WhatsappShareButton>
+                      <LinkedinShareButton
+                        url={`${process.env.REACT_APP_API}/${newURL}`}
+                        className="custom-share-button"
+                      >
+                        <LinkedinIcon size={35} className="custom-share-icon" />
+                        <p>Linkedin</p>
+                      </LinkedinShareButton>
+                      <EmailShareButton
+                        url={`${process.env.REACT_APP_API}/${newURL}`}
+                        className="custom-share-button"
+                      >
+                        <EmailIcon size={35} className="custom-share-icon" />
+                        <p>Email</p>
+                      </EmailShareButton>
+                    </VStack>
+                  </Box>
+                )}
+              </>
+            )}
+          </Flex>
+        </VStack>
+      </form>
     </Container>
   );
 }
